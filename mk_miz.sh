05f105421ec8005e686894b81f75a4fda7a62e92
mizarcvs <mizarcvs>
2008-03-05 23:09:27 +0000

    script creating the mizar html from a mizar distro; should be called with a param like ./mk_miz.sh 7.8.10_4.99.1005

---
+++ mk_miz.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# $1= 7.8.10_4.99.1005
+# script creating the mizar html from a mizar distro
+# current dir should contain the Makefile for doing this,
+# and all prerequisities required by the Makefile have to be present 
+wget ftp://mizar.uwb.edu.pl/pub/system/i386-linux/mizar-$1-i386-linux.tar
+mkdir $1
+tar xf mizar-$1-i386-linux.tar -C$1 
+cd $1
+tar xzf mizshare.tar.gz 
+tar xzf mizdoc.tar.gz 
+mkdir bin
+tar xzf mizbin.tar.gz -Cbin
+mkdir html 
+cp ../Makefile .
+cp -a mml miztmp
+cp Makefile  miztmp
+export MIZFILES=`pwd`
+cd miztmp
+make -j4 allacc | tee 00acc.log
+make -j4 allxml | tee 00xml.log
+make -j4 allxml1 | tee 00xml1.log
+make -j4 allhtmla1 | tee 00htmla1.log
+make hidden.acc
+make hidden.xml
+make hidden.xml1
+make hidden.htmla1
+make tarski.acc
+make tarski.xml
+make tarski.xml1
+make tarski.htmla1
+for i in `ls *.htmla1| sed -e 's/.htmla1//'`; do mv $i.htmla1 ../html/$i.html; done
+cd ..
+tar czf html_abstr.$i.noproofs.tar.gz html
+mv miztmp/proofs html
+tar czf html_abstr.$i.tar.gz html
