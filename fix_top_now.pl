6787895f95ccb31930acada6ce155d587df31527
Josef Urban <Josef.Urban@gmail.com>
2012-01-10 23:38:24 +0100

    fixes

--- fix_top_now.pl
+++ fix_top_now.pl
@@ -14,26 +14,31 @@
 # grep -B1 '<Now ' *.xml | grep -A1 'l\-$' | grep Now> 00N1
 #
 
-$told=""; $kold=0; @n=(); while (<>) { m/(.*).xml:.*line=.(\d+)/ or die;
-
-($t,$k)=($1,$2);
-if($told ne $t)
+$oldart=""; $oldlinenr=0; @l = @newlines=(); 
+while (<>)
 {
-    if($#l > 0) { open(F1,">new/$told.miz"); print F1 (@n, @l[$kold .. $#l]); close(F1);}
-    @n=(); $kold=0; $told = $t;
-    open(F,"$t.miz") or die "$t"; @l=<F>; close F;
+
+  m/(.*).xml:.*line=.(\d+)/ or die;
+  ($article,$linenr)=($1,$2);
+
+  # read the new article, finish the old if exists
+  if($oldart ne $article)
+  {
+      if($#l > 0) { open(F1,">new2/$oldart.miz"); print F1 (@newlines, @l[$oldlinenr .. $#l]); close(F1);}
+      @newlines=(); $oldlinenr=0; $oldart = $article;
+      open(F,"$article.miz") or die "$article"; @l=<F>; close F;
+  }
+
+  ($l[$linenr-1] =~ m/^(.*)\bnow(.*)/) or die $l[$linenr-1];
+  my ($before,$rest) = ($1,$2);
+
+  if($before =~ m/^ *([\w]+): */) {$label=$1; $bound = $linenr-2;}
+  elsif($l[$linenr-2] =~ m/^ *([\w]+): */) { $label=$1; $bound = $linenr-3;}
+  else { die "$article:$linenr:$l[$linenr-2]"; }
+  my $new = `grep -m1 "$label\\b" $article.htmla1 |sed -e "s/<[/]*a[^>]*>//g;" | sed -e "s/[&]amp;/\\&/g" |html2text`;
+  $new =~ s/.*thesis://;
+  push(@newlines,(@l[$oldlinenr .. $bound], $label, ":", $new, "proof ", $rest));
+  $oldlinenr = $linenr;
 }
-($l[$k-1] =~ m/^(.*)\bnow(.*)/) or die $l[$k-1];
-($b,$o) = ($1,$2);
-if($b =~ m/^ *([\w]+): */) {$c=$1; $bound = $k-2;}
-elsif($l[$k-2] =~ m/^ *([\w]+): */) { $c=$1; $bound = $k-3;}
-else { die "$t:$k:$l[$k-2]"; }
 
-if($l[$k-1] =~ m/^ *([\w]+): *now(.*)/)
-{
-($c,$o)=($1,$2); $new=`grep "$c" $t.htmla1 |sed -e "s/<[/]*a[^>]*>//g;" | sed -e "s/[&]amp;/\\&/g" |html2text`;
-$new =~ s/.*thesis://;
-push(@n,(@l[$kold .. $bound], $c, ":", $new, "proof ", $o));
-$kold = $k;
-}}
-open(F1,">new/$told.miz"); print F1 (@n, @l[$kold .. $#l]); close(F1);
+open(F1,">new2/$oldart.miz"); print F1 (@newlines, @l[$oldlinenr .. $#l]); close(F1);
